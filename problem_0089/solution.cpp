b553f054d40785745a99d9a5b370407fab862d4d
Long Le <drlongle@gmail.com>
2017-11-10 16:49:42 -0500

    Delete tabs

--- problem_0089/solution.cpp
+++ problem_0089/solution.cpp
@@ -69,12 +69,12 @@ struct node {
 int Solution::solve(const vector<int>& A){
     int n = A.size();
     int sum = 0;
-	
+
     for(int i=0; i<n; i++) sum+=A[i];
-	
+
     //knapsack algorithm for capacity sum/2
     int temp = sum/2;
-	
+
     for(int i=0; i<=n; i++){
 	for(int j=0; j<=temp; j++){
 	    if(i==0 || j==0) dp[i][j] = {0, 0};
@@ -84,7 +84,7 @@ int Solution::solve(const vector<int>& A){
 		if(j-A[i-1] >= 0){
 		    int curr_weight = dp[i-1][j-A[i-1]].weight + A[i-1];
 		    int curr_items = dp[i-1][j-A[i-1]].items + 1;
-					
+
 		    if((curr_weight>prev_weight) ||
                         ((curr_weight==prev_weight) && (curr_items<prev_items))){
 			dp[i][j] = {curr_items, curr_weight};
