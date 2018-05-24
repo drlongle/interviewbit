07db1b90f86e4ba8654f10225ef3902abf55b198
Long Le <drlongle@gmail.com>
2018-05-24 04:52:07 -0400

    Add another solution

---
+++ problem_0082/other.cpp
@@ -0,0 +1,31 @@
+int rec(vector<vector<int > > &dp, int id, int sum) {
+    if(sum < 0) return 0;
+    if(id == 0 && sum == 0) return 1;
+    if(id == 0) return 0;
+
+    if(dp[id][sum] != -1) return dp[id][sum];
+
+    int ans = 0;
+    for(int i = 0; i < 10; i++) {
+        ans += rec(dp,id - 1, sum - i);
+        ans %= 1000000007;
+    }
+    return dp[id][sum] = ans;
+}
+
+int Solution::solve(int A, int B) {
+    int ans = 0;
+    vector<vector<int> > dp;
+    dp.resize(A+1);
+    for(int i = 0; i < A+1; i++) {
+        dp[i].resize(B+1);
+        for(int j = 0; j < B+1; j++)
+              dp[i][j] = -1;
+    }
+    for(int i = 1; i < 10; i++) {
+        ans += rec(dp, A-1, B-i);
+        ans %= 1000000007;
+    }
+    return ans;
+}
+
